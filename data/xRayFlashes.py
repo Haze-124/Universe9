915ade6794736ec7b8bba516a6d4fd593a4ef602
Aya46 <ayabasketball0046@yahoo.co.jp>
2023-03-29 13:32:39 +1000

    new change

--- data/xRayFlashes.py
+++ data/xRayFlashes.py
@@ -9,8 +9,8 @@ import numpy as np
 import pandas as pd
 
 # Open csv file with flash data
-datapath = os.getcwd() + "\\data"
-flashdata = pd.read_csv(datapath + f'\\Flash_Data.csv', delimiter=',') 
+datapath = os.getcwd() + "/data"
+flashdata = pd.read_csv(datapath + f'/Flash_Data.csv', delimiter=',') 
 flash_velocity_data = []
 # For each flash
 for flash in flashdata.values:
@@ -19,17 +19,17 @@ for flash in flashdata.values:
     y = flash[3]
     # Look at direction and open corresponding galaxy-csv file
     if flash[1] == 'Top':
-        galaxydata = pd.read_csv(datapath + f'\\Top\\Distant_Galaxy_Data.csv', delimiter=',') 
+        galaxydata = pd.read_csv(datapath + f'/Top/Distant_Galaxy_Data.csv', delimiter=',') 
     elif flash[1] == 'Bottom':
-        galaxydata = pd.read_csv(datapath + f'\\Bottom\\Distant_Galaxy_Data.csv', delimiter=',') 
+        galaxydata = pd.read_csv(datapath + f'/Bottom/Distant_Galaxy_Data.csv', delimiter=',') 
     elif flash[1] == 'Right':
-        galaxydata = pd.read_csv(datapath + f'\\Right\\Distant_Galaxy_Data.csv', delimiter=',') 
+        galaxydata = pd.read_csv(datapath + f'/Right/Distant_Galaxy_Data.csv', delimiter=',') 
     elif flash[1] == 'Left':
-        galaxydata = pd.read_csv(datapath + f'\\Left\\Distant_Galaxy_Data.csv', delimiter=',') 
+        galaxydata = pd.read_csv(datapath + f'/Left/Distant_Galaxy_Data.csv', delimiter=',') 
     elif flash[1] == 'Back':
-        galaxydata = pd.read_csv(datapath + f'\\Back\\Distant_Galaxy_Data.csv', delimiter=',') 
+        galaxydata = pd.read_csv(datapath + f'/Back/Distant_Galaxy_Data.csv', delimiter=',') 
     elif flash[1] == 'Front':
-        galaxydata = pd.read_csv(datapath + f'\\Front\\Distant_Galaxy_Data.csv', delimiter=',') 
+        galaxydata = pd.read_csv(datapath + f'/Front/Distant_Galaxy_Data.csv', delimiter=',') 
     else:
         print(f"No match. flash direction = {flash[1]}")
     # Find the galaxy with coordinates closest to the flash's coords
